75fda14949416cc8724a672acf8c250b08597f67
LucaOcchio99 <occhionorelli.luca99@gmail.com>
2021-11-12 08:55:11 +0100

    end

---
+++ main/main.js
@@ -0,0 +1,13 @@
+//Stampare a schermo un messaggio all'interno di un h1,
+//utilizzando i data di Vue
+
+//Bonus
+//Aggiungre alla pagina un'immagne presa anch'essa da un data
+
+const app = new Vue({
+    el : '#app',
+    data : {
+        message : 'Ciao Vue',
+        immagine : 'https://www.soniaperonaci.it/wp-content/uploads/2020/11/Panzerotti-alla-pugliese-quadrata-anteprima-960x960.jpg'
+    }
+})
